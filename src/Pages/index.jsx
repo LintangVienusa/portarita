caf9780124f6856a7d4d3cc4dad12e61c6090b93
SoeKlontank <vienusa6661@gmail.com>
2022-12-29 15:44:52 +0700

    Change resource from https to http

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -9,7 +9,7 @@ export default function Maincomp () {
     const [filteredNews, setFilteredNews] = useState([])
 
     useEffect ( () => {
-        var url = 'https://newsapi.org/v2/top-headlines?country=id&apiKey=4b7de77b289f46409a505edad4e9aa50';
+        var url = 'http://newsapi.org/v2/top-headlines?country=id&apiKey=4b7de77b289f46409a505edad4e9aa50';
         var req = new Request(url);
         fetch(req)
         .then(response => response.json())
@@ -20,7 +20,7 @@ export default function Maincomp () {
     },[])
 
     function searchnews() {
-        var url = 'https://newsapi.org/v2/everything?q='+search+'&from=2022-12-08&sortBy=popularity&apiKey=4b7de77b289f46409a505edad4e9aa50';
+        var url = 'http://newsapi.org/v2/everything?q='+search+'&from=2022-12-08&sortBy=popularity&apiKey=4b7de77b289f46409a505edad4e9aa50';
         var req = new Request(url);
         fetch(req)
         .then(response => response.json())
