83d31c5a915e71f1030f90e567aee3ee4fd4b491
LintangVienusa <vienusa6661@gmail.com>
2023-02-07 01:33:03 +0700

    refactoring using  functional component, useEffect to fetch article, searching news, and set filtered news

--- src/Components/Body/navbar.jsx
+++ src/Components/Body/navbar.jsx
@@ -3,14 +3,14 @@ import './Styling/style.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Navbar } from "react-bootstrap";
 
-export default class MyNav extends React.Component {
-    render() {
-        return (
-            <Navbar expand="lg" variant="dark" className="navBar">
-                <Container>
-                    <Navbar.Brand href="#"><h3><b>Portarita</b></h3></Navbar.Brand>
-                </Container>
-            </Navbar>
-        )
-    }
+const MyNav = () => {
+    return (
+        <Navbar expand="lg" variant="dark" className="navBar">
+            <Container>
+                <Navbar.Brand href="#"><h3><b>Portarita</b></h3></Navbar.Brand>
+            </Container>
+        </Navbar>
+    )
 }
+
+export default MyNav;
